e91317ac7fa9d06d03d2bb666bad28850225aa4d
babajideawoseemo <akinloluolaposi@gmail.com>
2023-03-09 05:19:39 -0800

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     import sys
-    print_arg(sys.argv)
+    arg = len(sys.argv)
+    if arg == 1:
+        print('0 arguments.')
+    elif arg == 2:
+        print('1 argument:')
+        print('1: {}'.format(sys.argv[1]))
+    else:
+        print('{} arguments:'.format(arg - 1))
+        for arg in range(len(sys.argv) - 1):
+            print('{}: {}'.format(arg + 1, sys.argv[arg + 1]))
