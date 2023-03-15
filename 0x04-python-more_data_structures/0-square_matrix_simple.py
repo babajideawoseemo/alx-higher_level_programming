cdfbf0f939e0a18756a7844f587885e491af6516
babajideawoseemo <awoseemobabajide@gmail.com>
2023-03-15 19:28:40 +0000

    square a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/bin/usr/python3
+
+def square_matrix_simple(matrix=[]):
+    jide_matrix = [[x ** 2 for x in row] for row in matrix]
+    return jide_matrix
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+more on data structure for list, stack, queue
